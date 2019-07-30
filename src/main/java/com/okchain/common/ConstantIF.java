add073640d18066c50a15b052a19ae199e945e9d
lei.ji@okcoin.com <lei.ji@okcoin.net>
2019-07-30 11:29:24 +0800

    add keystore uml,modify api path

--- src/main/java/com/okchain/common/ConstantIF.java
+++ src/main/java/com/okchain/common/ConstantIF.java
@@ -7,34 +7,36 @@ public interface ConstantIF {
     //String HD_PATH = "M/44H/118H/0H/0/0";
     String HD_PATH = "M/44H/996H/0H/0/0";
     //url path
-    String ACCOUNT_URL_PATH = "/auth/accounts/";
-    String TRANSACTION_URL_PATH = "/txs";
+    String REST_PATH_PREFIX = "/okchain";
 
-    String GET_ACCOUNT_ALL_TOKENS_URL_PATH = "/accounts/";
+    String ACCOUNT_URL_PATH = REST_PATH_PREFIX + "/auth/accounts/";
+    String TRANSACTION_URL_PATH = REST_PATH_PREFIX + "/txs";
 
-    String GET_ACCOUNT_TOKEN_URL_PATH = "/accounts/";
+    String GET_ACCOUNT_ALL_TOKENS_URL_PATH = REST_PATH_PREFIX + "/accounts/";
 
-    String GET_TOKENS_URL_PATH = "/tokens";
+    String GET_ACCOUNT_TOKEN_URL_PATH = REST_PATH_PREFIX + "/accounts/";
 
-    String GET_TOKEN_URL_PATH = "/token/";
+    String GET_TOKENS_URL_PATH = REST_PATH_PREFIX + "/tokens";
 
-    String GET_PRODUCTS_URL_PATH = "/products";
+    String GET_TOKEN_URL_PATH = REST_PATH_PREFIX + "/token/";
 
-    String GET_DEPTHBOOK_URL_PATH = "/order/depthbook";
+    String GET_PRODUCTS_URL_PATH = REST_PATH_PREFIX + "/products";
 
-    String GET_CANDLES_URL_PATH = "/candles/";
+    String GET_DEPTHBOOK_URL_PATH = REST_PATH_PREFIX + "/order/depthbook";
 
-    String GET_TICKERS_URL_PATH = "/tickers";
+    String GET_CANDLES_URL_PATH = REST_PATH_PREFIX + "/candles/";
 
-    String GET_ORDERLIST_OPEN_URL_PATH = "/order/list/open";
+    String GET_TICKERS_URL_PATH = REST_PATH_PREFIX + "/tickers";
 
-    String GET_ORDERLIST_CLOSED_URL_PATH = "/order/list/closed";
+    String GET_ORDERLIST_OPEN_URL_PATH = REST_PATH_PREFIX + "/order/list/open";
 
-    String GET_DEALS_URL_PATH = "/deals";
+    String GET_ORDERLIST_CLOSED_URL_PATH = REST_PATH_PREFIX + "/order/list/closed";
 
-    String GET_TRANSACTIONS_URL_PATH = "/transactions";
+    String GET_DEALS_URL_PATH = REST_PATH_PREFIX + "/deals";
 
-    String GET_MATCHES_URL_PATH = "/matches";
+    String GET_TRANSACTIONS_URL_PATH = REST_PATH_PREFIX + "/transactions";
+
+    String GET_MATCHES_URL_PATH = REST_PATH_PREFIX + "/matches";
 
     //RPC transaction send mode
     String TX_SEND_MODE_BLOCK = "block";
