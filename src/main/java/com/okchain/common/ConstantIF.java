e7c7b837cdb2480b120f83bf66c6f5758e65a153
michael.w <guanyi.wang@okcoin.net>
2019-08-14 13:00:57 +0800

    javasdk-rpc V0.0.1

--- src/main/java/com/okchain/common/ConstantIF.java
+++ src/main/java/com/okchain/common/ConstantIF.java
@@ -2,11 +2,10 @@ package com.okchain.common;
 
 public interface ConstantIF {
     String CHAIN_ID = "okchain";
+
     String ADDRESS_PREFIX = "okchain";
 
     String HD_PATH = "M/44H/996H/0H/0/0";
-    //url path
-    String REST_PATH_PREFIX = "/okchain/v1";
 
     //RPC transaction send mode
     String TX_SEND_MODE_BLOCK = "block";
