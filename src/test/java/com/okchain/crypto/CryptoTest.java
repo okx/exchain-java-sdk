4a7d33c9a5c3cc6a5b783b895db1a7408eeec7c2
William Liu <shengwei.liu@okcoin.net>
2020-03-06 18:54:49 +0800

    Revision Test Case.

--- src/test/java/com/okchain/crypto/CryptoTest.java
+++ src/test/java/com/okchain/crypto/CryptoTest.java
@@ -15,34 +15,35 @@ import com.okchain.crypto.io.cosmos.util.AddressUtil;
 import java.util.Base64;
 
 public class CryptoTest {
+    private static String TEST_PRIVATE_KEY = "c4c451ce673485521f9c9b74b6d90f0da433ef7f012fa7f9db4def627dccd632";
+    private static String TEST_MNEMONIC = "total lottery arena when pudding best candy until army spoil drill pool";
+
     @Test
-    public void testGeneratePrivateKey() {
+    public void generatePrivateKeyTest() {
         String priv = Crypto.generatePrivateKey();
-        System.out.println(priv);
+        Assert.assertNotEquals(TEST_PRIVATE_KEY,priv);
     }
 
     @Test
-    public void testGenerateAddress() {
-//        String priv = Crypto.generatePrivateKey();
-        String priv = "c4c451ce673485521f9c9b74b6d90f0da433ef7f012fa7f9db4def627dccd632";
+    public void generateAddressTest() {
         long startTime = System.currentTimeMillis();
-        byte[] pub = Crypto.generatePubKeyFromPriv(priv);
-        System.out.println(Hex.toHexString(pub));
+        byte[] pub = Crypto.generatePubKeyFromPriv(TEST_PRIVATE_KEY);
+        Assert.assertEquals("02fa42bcdb80aae828e480d80ec05c3a8847a2dc150071d08adf2f99fbcb09a3fb",Hex.toHexString(pub));
         try {
             String addr = AddressUtil.createNewAddressSecp256k1("okchain", pub);
-            System.out.println(addr);
+            Assert.assertEquals("okchain152p8xmejhza7wuhhzut88vkakdgasqwlw2qjcf",addr);
         }catch (Exception e){
             e.printStackTrace();
         }
 
         long endTime = System.currentTimeMillis();
         float excTime = (float) (endTime - startTime) / 1000;
-        System.out.println("执行时间：" + excTime + "s");
+        System.out.println("execution time：" + excTime + "s");
 
     }
 
     @Test
-    public void testVerify() {
+    public void verifyTest() {
         String priv = Crypto.generatePrivateKey();
         byte[] msg = new String("hello").getBytes();
         try {
@@ -58,21 +59,21 @@ public class CryptoTest {
     }
 
     @Test
-    public void generateMnemonic() {
-        System.out.println(Crypto.generateMnemonic());
+    public void generateMnemonicTest() {
+        Assert.assertNotEquals(TEST_MNEMONIC,Crypto.generateMnemonic());
     }
 
     @Test
-    public void generatePrivateKeyFromMnemonic() {
+    public void generatePrivateKeyFromMnemonicTest() {
         String mnemonic = "total lottery arena when pudding best candy until army spoil drill pool";
         String priv = Crypto.generatePrivateKeyFromMnemonic(mnemonic);
-        System.out.println(priv);
-        System.out.println(Crypto.generateAddressFromPriv(priv));
+        Assert.assertEquals("29892b64003fc5c8c89dc795a2ae82aa84353bb4352f28707c2ed32aa1011884",priv);
+        Assert.assertEquals("okchain1g7c3nvac7mjgn2m9mqllgat8wwd3aptdqket5k",Crypto.generateAddressFromPriv(priv));
     }
 
 
     @Test
-    public void generateKeyStore() {
+    public void generateKeyStoreTest() {
         File file = new File("./");
         String fileName = "";
         try {
@@ -94,12 +95,12 @@ public class CryptoTest {
     }
 
     @Test
-    public void getPrivatekeyFromKeyStore() {
+    public void getPrivatekeyFromKeyStoreTest() {
         File file = new File("./");
         String password = "1234567";
         String filename = "";
         try {
-            filename = KeyStoreUtils.generateWalletFile(password, "c4c451ce673485521f9c9b74b6d90f0da433ef7f012fa7f9db4def627dccd632", file, true);
+            filename = KeyStoreUtils.generateWalletFile(password, TEST_PRIVATE_KEY, file, true);
         } catch (CipherException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -107,7 +108,7 @@ public class CryptoTest {
         }
         try {
             String privateKey = KeyStoreUtils.getPrivateKeyFromKeyStoreFile(filename, password);
-            System.out.println(privateKey);
+            Assert.assertEquals(TEST_PRIVATE_KEY,privateKey);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (CipherException e) {
@@ -120,7 +121,7 @@ public class CryptoTest {
 
 
     @Test
-    public void validPubKey() {
+    public void validPubKeyTest() {
         String priv = Crypto.generatePrivateKey();
         String pub = Crypto.generatePubKeyHexFromPriv(priv);
         Assert.assertTrue(Crypto.validPubKey(pub));
