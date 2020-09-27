1f7e4a57a570f07f40d7da9324b6d5f0c10c0944
oker <oker@okerdeMacBook-Pro.local>
2020-09-27 14:42:31 +0800

    add deposit function

--- src/main/java/io/cosmos/msg/MsgDeposit.java
+++ src/main/java/io/cosmos/msg/MsgDeposit.java
@@ -2,7 +2,9 @@ package io.cosmos.msg;
 
 import io.cosmos.common.EnvInstance;
 import io.cosmos.msg.utils.Message;
+import io.cosmos.msg.utils.type.MsgDepositValue;
 import io.cosmos.msg.utils.type.MsgEditValidatorValue;
+import io.cosmos.types.DecCoin;
 import io.cosmos.types.Description;
 
 public class MsgDeposit  extends MsgBase {
@@ -10,9 +12,34 @@ public class MsgDeposit  extends MsgBase {
         EnvInstance.setEnv("okq");
 
         MsgDeposit msg = new MsgDeposit();
-        msg.setMsgType("cosmos-sdk/MsgEditValidator");
+        msg.setMsgType("okexchain/staking/MsgDeposit");
         msg.initMnemonic(EnvInstance.getEnv().GetNode0Mnmonic());
 
+        Message messages = msg.produceMsg("okt", "10.00000000", "okexchain10q0rk5qnyag7wfvvt7rtphlw589m7frsku8qc9");
+
+        msg.submit(messages, "6.00000000", "200000", "");
+    }
+
+    public Message produceMsg(String denom, String amountDenom, String delegrator) {
+
+        DecCoin d = new DecCoin();
+        d.setDenom(denom);
+        d.setAmount(amountDenom);
+
+        MsgDepositValue value = new MsgDepositValue();
+
+        System.out.println("this.operAddress:");
+        System.out.println(this.operAddress);
+
+        value.setAmount(d);
+        value.setDelegatorAddress(delegrator);
+
+        System.out.println("println this.operAdddress");
+
+        Message<MsgDepositValue> msg = new Message<>();
+        msg.setType(msgType);
+        msg.setValue(value);
+        return msg;
     }
 
 }
