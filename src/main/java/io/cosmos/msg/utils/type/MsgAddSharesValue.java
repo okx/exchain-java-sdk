0f7decce1ae4fc7f0c4719218f79aaf7f67716bb
Haitao Cao <caohaitao_linux@buaa.edu.cn>
2020-09-28 18:55:18 +0800

    add vote shares function (#34)
    * add deposit and withdraw staking function
    
    * add vote shares function
    
    Co-authored-by: oker <oker@okerdeMacBook-Pro.local>

---
+++ src/main/java/io/cosmos/msg/utils/type/MsgAddSharesValue.java
@@ -0,0 +1,39 @@
+package io.cosmos.msg.utils.type;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder(alphabetic = true)
+public class MsgAddSharesValue {
+    @JsonProperty("delegator_address")
+    @SerializedName("delegator_address")
+    private String delAddr;
+
+    @JsonProperty("validator_addresses")
+    @SerializedName("validator_addresses")
+    private String [] valAddrs;
+
+    public void setDelAddr(String delAddr) {
+        this.delAddr = delAddr;
+    }
+
+    public void setValAddrs(String [] valAddrs) {
+        this.valAddrs = valAddrs;
+    }
+
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("delegator_address", this.delAddr)
+                .append("validator_addresses", this.valAddrs)
+                .toString();
+    }
+
+}
+
