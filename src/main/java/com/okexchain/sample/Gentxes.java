43539e521b2d17d8889168deba333662849fc31f
zhengchao <chao.zheng@okcoin.com>
2020-10-10 20:19:58 +0800

    commit 4 node

--- src/main/java/com/okexchain/sample/Gentxes.java
+++ src/main/java/com/okexchain/sample/Gentxes.java
@@ -30,17 +30,21 @@ public class Gentxes {
     public static void main(String[] args) {
 
         String[][] validators = {
-                {"03d9d7b471f537aaeeeadfbebf863f0a1e4c518fd4f5954b189bfb6079c52881f1",
-                        "okexchainvalconspub1zcjduepqtv2yy90ptjegdm34vfhlq2uw9eu39hjrt98sffj7yghl4s47xv7svt56mk",
-                        "moniker",  "website", "identity", "details",},
+                {"03425b43cbe9e7e50bd9a1c114d59d73cf28ef0ac7fcdb8c86ac39b1641cdd60dd",
+                        "okexchainvalconspub1zcjduepq4zcj4e2m6h6tm76ucrvkn5drxa6j2qt5s8qgf0ajwa6kn2pdgksq5lvlcl",
+                        "OKEx",  "https://www.okex.com", "https://static.okex.com/cdn/assets/imgs/MjAxOTQ/5CAB3C0086E2F1135A3428116E435B8A.png", "The World's Leading One-Stop Crypto Exchange",},
 
-                {"03d9d7b471f537aaeeeadfbebf863f0a1e4c518fd4f5954b189bfb6079c52881f1",
-                        "okexchainvalconspub1zcjduepqtv2yy90ptjegdm34vfhlq2uw9eu39hjrt98sffj7yghl4s47xv7svt56mk",
-                        "moniker",  "website", "identity", "details",},
+                {"02a286cfc19239850191f8c94aa6ee4ced75716895536330db4883d6040fc15aed",
+                        "okexchainvalconspub1zcjduepqy6tlu8zak7vj20cpalng3e2hadv8gadwyk6zr2wr4chnr08at3mqc0d899",
+                        "OKLink",  "https://www.oklink.com", "https://static.bafang.com/cdn/assets/imgs/MjAyMDM/238A87F2CCB1F1B2184CB21DC55D0EA0.png", "World-Leading Blockchain Explorer",},
 
-                {"03d9d7b471f537aaeeeadfbebf863f0a1e4c518fd4f5954b189bfb6079c52881f1",
-                        "okexchainvalconspub1zcjduepqtv2yy90ptjegdm34vfhlq2uw9eu39hjrt98sffj7yghl4s47xv7svt56mk",
-                        "moniker",  "website", "identity", "details",},
+                {"029dddedc1abe0c7581040437f7226461bcd91b022c3cd2e6aed4e1dcf7f175591",
+                        "okexchainvalconspub1zcjduepq0ezwe9ev35v9ht370h3wkhqm33a3sc2ndwa0s2e7hj6zuw68qdnqcsfu0t",
+                        "OKEx PooL",  "https://www.okex.com/pool", "identity", "The World's Leading Blockchain Digital Asset Mining Platform",},
+
+                {"026c09a0de87e4eb4ae10e88af466a25499970e7408a9971ecb14c50f59081e474",
+                        "okexchainvalconspub1zcjduepqqaes4htn3vsan8c9c8hs90w2vyzk0d8hlxwupawxz4rhp6srg0ts0cvc4k",
+                        "OKEx DEX",  "https://www.okex.com/dex-test", "identity", "OKEx DEX Borderless decentralized trading platform",},
         };
 
         for (int i = 0; i < validators.length; i++) {
