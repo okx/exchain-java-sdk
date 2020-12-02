56c7df5680762056b3eeea994f0b212484127c85
ylsGit <>
2020-12-02 18:38:02 +0800

    remove useless code

--- src/main/java/com/okexchain/sample/ColdSign.java
+++ src/main/java/com/okexchain/sample/ColdSign.java
@@ -2,8 +2,6 @@ package com.okexchain.sample;
 
 import com.okexchain.env.EnvInstance;
 import com.okexchain.env.LocalEnv;
-import com.okexchain.utils.Utils;
-import com.okexchain.utils.crypto.AddressUtil;
 import com.okexchain.utils.crypto.PrivateKey;
 import com.okexchain.msg.MsgBase;
 import com.okexchain.msg.MsgSend;
@@ -15,18 +13,10 @@ import com.okexchain.msg.common.Signature;;
 public class ColdSign {
 
     public static void main(String[] args) {
-//        BigInteger privKey = new BigInteger("8145BFB1D3ACC216C54490952C994D5E3BCE09DD65AE73D0C79F892284F721E7", 16);
-//        BigInteger pubKey = Sign.publicKeyFromPrivate(privKey);
-//        String addressETH = Keys.getAddress(pubKey);
-//        String addressOK = AddressUtil.createNewAddressETHSecp256k1(EnvInstance.getEnv().GetMainPrefix(),Hex.decode(addressETH));
-
         EnvInstance.setEnv(new LocalEnv("http://localhost:8545"));
 
         PrivateKey key = new PrivateKey("8145bfb1d3acc216c54490952c994d5e3bce09dd65ae73d0c79f892284f721e7");
 //        PrivateKey key = new PrivateKey("puzzle glide follow cruel say burst deliver wild tragic galaxy lumber offer");
-//        PrivateKey key = new PrivateKey(privKey.toString(16),
-//                Numeric.toHexStringWithPrefixZeroPadded(pubKey, 64 << 1),
-//                addressOK);
 
         MsgSend msg = new MsgSend();
         msg.init(key.getAddress(), key.getPubKey());
