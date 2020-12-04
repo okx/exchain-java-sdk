1ce4a9b06c9305b08699207ee0b9673dcb7638ec
StupidTAO <2310852380@qq.com>
2020-12-04 15:58:25 +0800

    update staking test case

--- src/main/java/com/okexchain/sample/Staking.java
+++ src/main/java/com/okexchain/sample/Staking.java
@@ -28,13 +28,13 @@ public class Staking {
 //        System.out.println(prikey);
 
         EnvInstance.setEnv(new LocalEnv("http://localhost:8545"));
-        PrivateKey key = new PrivateKey("2525cc412a6887072f33371cfcc322e9071730636679d92d950c5a36b5cc66d7");
+        PrivateKey key = new PrivateKey("769CE08B6AAD66A579A1925F9D208570296EB505CC7819D7E6D7A0388EB4FD51");
 
         MsgBindProxy msg = new MsgBindProxy();
         msg.init(key.getAddress(), key.getPubKey());
 
-        String delegator_address = "okexchain1k248qk9w04r4atekae0tyv9k6d6a64xl6t8nac";
-        String proxy_address = "okexchain10q0rk5qnyag7wfvvt7rtphlw589m7frsku8qc9";
+        String delegator_address = "okexchain1tscazctcr3jvtdx4harrzm75xltlccrr36cw54";
+        String proxy_address = "okexchain1pt7xrmxul7sx54ml44lvv403r06clrdkgmvr9g";
 
         Message messages = msg.produceMsg(delegator_address, proxy_address);
 
@@ -58,7 +58,7 @@ public class Staking {
         MsgEditValidator msg = new MsgEditValidator();
         msg.init(key.getAddress(), key.getPubKey());
 
-        Message messages = msg.produceMsg("1","1","1","1", "okexchainvaloper10q0rk5qnyag7wfvvt7rtphlw589m7frshchly8");
+        Message messages = msg.produceMsg("1","1","1","1", "okexchainvaloper1pt7xrmxul7sx54ml44lvv403r06clrdkfluue2");
 
         try {
             UnsignedTx unsignedTx = msg.getUnsignedTx(messages,"0.01000000", "200000", "okexchain transfer!");
@@ -76,11 +76,11 @@ public class Staking {
 
     static void testMsgDeposit() {
         EnvInstance.setEnv(new LocalEnv("http://localhost:8545"));
-        PrivateKey key = new PrivateKey("8145bfb1d3acc216c54490952c994d5e3bce09dd65ae73d0c79f892284f721e7");
+        PrivateKey key = new PrivateKey("769CE08B6AAD66A579A1925F9D208570296EB505CC7819D7E6D7A0388EB4FD51");
         MsgDeposit msg = new MsgDeposit();
         msg.init(key.getAddress(), key.getPubKey());
 
-        Message messages = msg.produceMsg("okt", "10.00000000", "okexchain10q0rk5qnyag7wfvvt7rtphlw589m7frsku8qc9");
+        Message messages = msg.produceMsg("tokt", "10.000000000000000000", "okexchain1tscazctcr3jvtdx4harrzm75xltlccrr36cw54");
 
         try {
             UnsignedTx unsignedTx = msg.getUnsignedTx(messages,"0.01000000", "200000", "okexchain transfer!");
@@ -102,7 +102,7 @@ public class Staking {
         MsgWithdrawStaking msg = new MsgWithdrawStaking();
         msg.init(key.getAddress(), key.getPubKey());
 
-        Message messages = msg.produceMsg("okt", "10.00000000", "okexchain10q0rk5qnyag7wfvvt7rtphlw589m7frsku8qc9");
+        Message messages = msg.produceMsg("tokt", "10.00000000", "okexchain1pt7xrmxul7sx54ml44lvv403r06clrdkgmvr9g");
 
         try {
             UnsignedTx unsignedTx = msg.getUnsignedTx(messages,"0.01000000", "200000", "okexchain transfer!");
@@ -124,8 +124,8 @@ public class Staking {
         MsgAddShares msg = new MsgAddShares();
         msg.init(key.getAddress(), key.getPubKey());
 
-        String [] validators = {"okexchainvaloper10q0rk5qnyag7wfvvt7rtphlw589m7frshchly8"};
-        Message messages = msg.produceMsg("okexchain10q0rk5qnyag7wfvvt7rtphlw589m7frsku8qc9", validators);
+        String [] validators = {"okexchainvaloper1pt7xrmxul7sx54ml44lvv403r06clrdkfluue2"};
+        Message messages = msg.produceMsg("okexchain1pt7xrmxul7sx54ml44lvv403r06clrdkgmvr9g", validators);
 
 
         try {
@@ -148,7 +148,7 @@ public class Staking {
         MsgRegProxy msg = new MsgRegProxy();
         msg.init(key.getAddress(), key.getPubKey());
 
-        Message messages = msg.produceMsg("okexchain10q0rk5qnyag7wfvvt7rtphlw589m7frsku8qc9", true);
+        Message messages = msg.produceMsg("okexchain1pt7xrmxul7sx54ml44lvv403r06clrdkgmvr9g", true);
 
         try {
             UnsignedTx unsignedTx = msg.getUnsignedTx(messages,"0.01000000", "200000", "okexchain transfer!");
@@ -170,7 +170,7 @@ public class Staking {
         MsgUnjail msg = new MsgUnjail();
         msg.init(key.getAddress(), key.getPubKey());
 
-        Message messages = msg.produceMsg("okexchainvaloper10q0rk5qnyag7wfvvt7rtphlw589m7frshchly8");
+        Message messages = msg.produceMsg("okexchainvaloper1pt7xrmxul7sx54ml44lvv403r06clrdkfluue2");
 
         try {
             UnsignedTx unsignedTx = msg.getUnsignedTx(messages,"0.01000000", "200000", "okexchain transfer!");
