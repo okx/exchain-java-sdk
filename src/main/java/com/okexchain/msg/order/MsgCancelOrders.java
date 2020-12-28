7e8c469d40a6d5e3b77f14524f7f722783bdbee1
meng <805442788@qq.com>
2020-12-28 21:33:59 +0800

    remove order for sample

--- src/main/java/com/okexchain/msg/order/MsgCancelOrders.java
+++ src/main/java/com/okexchain/msg/order/MsgCancelOrders.java
@@ -1,13 +1,22 @@
 package com.okexchain.msg.order;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.okexchain.env.EnvInstance;
 import com.okexchain.msg.MsgBase;
 import com.okexchain.msg.common.Message;
+import com.okexchain.msg.tx.UnsignedTx;
+import com.okexchain.utils.crypto.PrivateKey;
+
+import java.util.ArrayList;
 
 public class MsgCancelOrders extends MsgBase {
 
-    public MsgCancelOrders() { setMsgType("okexchain/order/MsgCancel"); }
+    public MsgCancelOrders() {
+        setMsgType("okexchain/order/MsgCancel");
+    }
 
-    public Message produceMsg(String [] orderIDs){
+    public Message produceMsg(String[] orderIDs) {
 
         MsgCancelOrdersValue value = new MsgCancelOrdersValue();
         value.setSender(this.address);
@@ -17,5 +26,24 @@ public class MsgCancelOrders extends MsgBase {
         msg.setType(msgType);
         msg.setValue(value);
         return msg;
+    }
+
+    public static void main(String[] args) throws Exception {
+        EnvInstance.getEnv().setChainID("okexchainevm-8");
+        EnvInstance.getEnv().setRestServerUrl("http://localhost:8545");
+
+        PrivateKey key = new PrivateKey("EA6D97F31E4B70663594DD6AFC3E3550AAB5FDD9C44305E8F8F2003023B27FDA");
+        MsgCancelOrders msg = new MsgCancelOrders();
+        msg.init(key);
+
+        ArrayList<String> orderIDs = new ArrayList<>();
+        orderIDs.add("ID0000001373-1");
+        String[] ids = new String[orderIDs.size()];
+
+        Message messages = msg.produceMsg(orderIDs.toArray(ids));
+
+        JSONObject result = msg.submit(messages, "0.02000000", "200000", "");
+        boolean succeed = msg.isTxSucceed(result);
+        System.out.println("cancel orders: " + (succeed ? "succeed" : "failed"));
     }
 }
--- src/main/java/com/okexchain/msg/order/MsgNewOrders.java
+++ src/main/java/com/okexchain/msg/order/MsgNewOrders.java
@@ -1,13 +1,22 @@
 package com.okexchain.msg.order;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.okexchain.env.EnvBase;
+import com.okexchain.env.EnvInstance;
 import com.okexchain.msg.MsgBase;
 import com.okexchain.msg.common.Message;
+import com.okexchain.utils.crypto.PrivateKey;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MsgNewOrders extends MsgBase {
 
-    public MsgNewOrders() { setMsgType("okexchain/order/MsgNew"); }
+    public MsgNewOrders() {
+        setMsgType("okexchain/order/MsgNew");
+    }
 
     public Message produceMsg(List<OrderItem> orderItems) {
         MsgNewOrdersValue value = new MsgNewOrdersValue();
@@ -18,5 +27,35 @@ public class MsgNewOrders extends MsgBase {
         msg.setType(msgType);
         msg.setValue(value);
         return msg;
+    }
+
+    public String[] getOrderIDs(JSONObject result) throws Exception {
+        String orders = getMatchedAttribute(result, "orders");
+        JSONArray orderJSONs = JSONObject.parseObject(orders).getJSONArray("");
+        String[] orderIDs = new String[orderJSONs.size()];
+        for (int i = 0; i < orderJSONs.size(); i++) {
+            orderIDs[i] = orderJSONs.getJSONObject(i).getString("orderid");
+        }
+        return orderIDs;
+    }
+
+    public static void main(String[] args) throws Exception {
+        EnvInstance.getEnv().setChainID("okexchainevm-8");
+        EnvInstance.getEnv().setRestServerUrl("http://localhost:8545");
+
+        PrivateKey key = new PrivateKey("EA6D97F31E4B70663594DD6AFC3E3550AAB5FDD9C44305E8F8F2003023B27FDA");
+        MsgNewOrders msg = new MsgNewOrders();
+        msg.init(key);
+
+        List<OrderItem> orderItems = new ArrayList<>();
+        OrderItem item = new OrderItem("2", "usdk-5f7_okt", "2", "SELL");
+        orderItems.add(item);
+
+        Message messages = msg.produceMsg(orderItems);
+
+        JSONObject res = msg.submit(messages, "0.02000000", "200000", "");
+
+        String[] orderIDs = msg.getOrderIDs(res);
+        System.out.println(Arrays.toString(orderIDs));
     }
 }
