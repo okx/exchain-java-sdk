a2aa90a427d7599ecdc0655045f8b0e8e59708c0
meng <805442788@qq.com>
2020-12-31 20:16:50 +0800

    remove legacy

--- src/main/java/com/okexchain/legacy/common/jsonrpc/types/RPCError.java
+++
@@ -1,7 +0,0 @@
-package com.okexchain.legacy.common.jsonrpc.types;
-
-public class RPCError {
-    int code;
-    String message;
-    String data;
-}
--- src/main/java/com/okexchain/legacy/exception/OKEXChainException.java
+++
@@ -1,5 +0,0 @@
-package com.okexchain.legacy.exception;
-
-public class OKEXChainException extends RuntimeException {
-    public OKEXChainException(String msg) {super(msg);}
-}
