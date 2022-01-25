7005befa68e6b2e943f543d50502891baed27b28
xuqing-lee <82646011+xuqing-lee@users.noreply.github.com>
2022-01-25 23:45:03 +0800

    Merge PR: add MsgContractMethodBlockedListProposalTest  (#103)
    * public class MsgContractMethodBlockedListProposalTest {
    
    * changed the class name
    
    * add obj get method
    
    * obj to json
    
    * add code
    
    * add ManageContractMethodBlockedListProposal
    
    * fix MsgContractMethodBlockedListProposalValue json encode
    
    Co-authored-by: xiangjianmeng <805442788@qq.com>

---
+++ src/main/java/com/okexchain/msg/common/Contract_addresses.java
@@ -0,0 +1,34 @@
+package com.okexchain.msg.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.List;
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder(alphabetic = true)
+public class Contract_addresses {
+
+    @JsonProperty("address")
+    @SerializedName("address")
+    private String address;
+
+
+    @JsonProperty("block_methods")
+    @SerializedName("block_methods")
+    private List<Block_methods> block_methods;
+
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("address", this.address)
+                .append("block_methods", this.block_methods)
+                .toString();
+    }
+}
---
+++ src/test/java/com/okexchain/test/MsgContractMethodBlockedListProposalTest.java
@@ -0,0 +1,87 @@
+package com.okexchain.test;
+
+import com.alibaba.fastjson.JSON;
+import com.okexchain.msg.common.Block_methods;
+import com.okexchain.msg.common.Contract_addresses;
+import com.okexchain.msg.common.Message;
+import com.okexchain.msg.gov.MsgContractMethodBlockedListProposal;
+import com.okexchain.msg.gov.MsgContractMethodBlockedListProposalValue;
+import com.okexchain.msg.tx.UnsignedTx;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MsgContractMethodBlockedListProposalTest {
+
+    @Test
+    public void testMsgContractMethodBlockedListProposal(){
+
+        MsgContractMethodBlockedListProposalValue mb=new MsgContractMethodBlockedListProposalValue();
+
+
+
+
+
+        Block_methods blm=new Block_methods();
+        blm.setExtra("inc()");
+        blm.setSign("0x371303c0");
+
+        Block_methods blm1=new Block_methods();
+        blm1.setExtra("vote()");
+        blm1.setSign("0x37130345");
+
+        Block_methods blm2=new Block_methods();
+        blm2.setExtra("sedEHT()");
+        blm2.setSign("0x37000345");
+
+
+        List<Block_methods> bm=new ArrayList<>();
+        bm.add(blm);
+        bm.add(blm1);
+        bm.add(blm2);
+
+
+
+        Block_methods blm3=new Block_methods();
+        blm3.setExtra("query()");
+        blm3.setSign("0x9830345");
+
+        Block_methods blm4=new Block_methods();
+        blm4.setExtra("sedOKT()");
+        blm4.setSign("0x37000345");
+
+        List<Block_methods> bm1=new ArrayList<>();
+        bm1.add(blm3);
+        bm1.add(blm4);
+
+
+        Contract_addresses ca=new Contract_addresses();
+        ca.setAddress("0x6825415e61F95965d80C556709C0f32B4092fc25");
+        ca.setBlock_methods(bm);
+
+        Contract_addresses ca1=new Contract_addresses();
+        ca1.setAddress("0x90dAE493DAEE9B8A16EAf1D87dc553A48644Ccfe");
+        ca1.setBlock_methods(bm1);
+
+
+        List<Contract_addresses> contract_addresses_list=new ArrayList();
+        contract_addresses_list.add(ca);
+        contract_addresses_list.add(ca1);
+
+        mb.setTitle("title");
+        mb.setDescription("desc");
+        mb.setContract_addresses(contract_addresses_list);
+        mb.setAdded(true);
+
+
+        MsgContractMethodBlockedListProposal msg = new MsgContractMethodBlockedListProposal();
+
+        Message messages = msg.produceContractMethodBlockedListProposal(mb,"","");
+
+        UnsignedTx unsignedTx = msg.getUnsignedTx(messages,"0.05000000","500000","");
+
+
+        System.out.println(JSON.toJSONString(mb));
+    }
+}
