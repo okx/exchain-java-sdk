6db678a3e27c490bff6af6bf5c8f54f6c891ebd7
lixuqing <xuqing.li@okg.com>
2022-10-24 11:55:09 +0800

    update feesplit query API

--- src/main/java/com/okexchain/msg/feesplit/FeesplitQuery.java
+++ src/main/java/com/okexchain/msg/feesplit/FeesplitQuery.java
@@ -9,10 +9,15 @@ public class FeesplitQuery {
     //%s=contractAddr
     private static final String FEESPLIT_CONTRACT = "/feesplit/contract/%s";
 
-
     //%s=withdrawer addr
-    private static final String FEESPLIT_WITHDRAWER = "/feesplit/withdrawer-contracts/%s";
+    //%s=page
+    //%s=limit
+    private static final String FEESPLIT_WITHDRAWER = "/feesplit/withdrawer/%s?page=%s&limit=%s";
 
+    //%s=deployer addr
+    //%s=page
+    //%s=limit
+    private static final String FEESPLIT_DEPLOYER="/feesplit/deployer/%s?page=%s&limit=%s";
 
     //query Feesplit Info By ContractAddr
     public static String queryFeesplitInfoByContractAddr(String contractAddr) {
@@ -25,9 +30,19 @@ public class FeesplitQuery {
     }
 
 
+    //query Contract Addr By deployer Addr
+    public static String queryContractAddrBydeployerAddr(String deployerAddr, int page, int limit){
+        String basePath = String.format(FEESPLIT_DEPLOYER, deployerAddr, page, limit);
+        Result res = HttpUtils.httpGetResult(EnvInstance.getEnv().GetRestServerUrl() + EnvInstance.getEnv().GetRestPathPrefix() + basePath);
+        if (res.isSuccess()) {
+            return res.getData();
+        }
+        return null;
+    }
+
     //query Contract Addr By Withdrawer Addr
-    public static String queryContractAddrByWithdrawerAddr(String withdrawerAddr) {
-        String basePath = String.format(FEESPLIT_WITHDRAWER, withdrawerAddr);
+    public static String queryContractAddrByWithdrawerAddr(String withdrawerAddr, int page, int limit) {
+        String basePath = String.format(FEESPLIT_WITHDRAWER, withdrawerAddr, page, limit);
         Result res = HttpUtils.httpGetResult(EnvInstance.getEnv().GetRestServerUrl() + EnvInstance.getEnv().GetRestPathPrefix() + basePath);
         if (res.isSuccess()) {
             return res.getData();
--- src/main/java/com/okexchain/sample/FeesplitQuerySample.java
+++ src/main/java/com/okexchain/sample/FeesplitQuerySample.java
@@ -10,6 +10,7 @@ public class FeesplitQuerySample {
     public static void main(String[] args) {
         EnvInstance.getEnvTestNet();
         System.out.println(FeesplitQuery.queryFeesplitInfoByContractAddr("0x95d85EC4003A349c05107A0362e846Ca157194A0"));
-        System.out.println(FeesplitQuery.queryContractAddrByWithdrawerAddr("ex17d7yg0n5uhqtke865c9t0rehmkjg736c7j47as"));
+        System.out.println(FeesplitQuery.queryContractAddrByWithdrawerAddr("ex17d7yg0n5uhqtke865c9t0rehmkjg736c7j47as",1,2));
+        System.out.println(FeesplitQuery.queryContractAddrBydeployerAddr("ex1v8segh8mlw297s2ksy6pp6nwxn3el0wmkuqsx2",1,2));
     }
 }
